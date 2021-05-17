69858a1e45b008c7bc6b4379ae314926cd3271d3
vishal garg <=>
2021-05-17 12:01:06 +0530

    first

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import './index.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronRight, faChevronLeft, faCircle, faCheckCircle, faPlus } from '@fortawesome/free-solid-svg-icons';
 import jsPDF from 'jspdf'
+import myImage from './image/myImage.jpg'
+import Popup from 'reactjs-popup';
 import 'jspdf-autotable'
  //...............................
  
@@ -34,27 +36,42 @@ const App = () => {
     fetchData();
   }, []);
    const jsPdfGenerator=()=>
-  {
+  { 
     var doc = new jsPDF('p','pt');
-    doc.text(250,100,'INVOICE');
+
+    doc.text(250,20,'INVOICE');
+    doc.line(250, 21, 315, 21);
+   
+    doc.text(50,40,'FROM:')
+    doc.text(50,65,'VISHAL GARG')
+    doc.text(50,80,'INDIA')
+    doc.text(50,100,'1234567891')
+    doc.line(50, 110, 250, 110);
+    doc.line(250, 35, 250, 200);
+    var today = new Date();
+    var newdat = "Date: "+ today.getDate()+ "-"+ (today.getMonth()+1) +"-"+today.getFullYear();
+    doc.text(450,60,newdat);
+    var ne= "Time: " + today.getHours()+ ":"+today.getMinutes()+":"+today.getSeconds();
+    doc.text(450,80,ne);
+    
+    doc.addImage(myImage, 'JPG', 400, 10, 200, 30);
     doc.text(50,130,'BILL TO:')
     doc.text(50,155,person.name.first)
     doc.text(50,175,person.location.city)
+    doc.text(50,195,person.phone)
 
     var col1 = ["ITEM NAME", "PRICE","ITEM QUANTITY","TOTAL PRICE"];
      var rows1 = [];
    items.forEach(element => {      
-      console.log("nu");
-      console.log(element);
         var temp1 = [element.itemName,element.price,element.quantity,element.price*element.quantity];
        console.log(temp1);
         rows1.push(temp1);
 
-    });    
+    });  
 
-    doc.autoTable(col1, rows1, { startY: 200 }); 
-     doc.text(250,250+30*rows1.length,'TOTAL ITEM: '+totalItemCount);
-    doc.text(250,270+30*(rows1.length),'TOTAL PRICE: '+totalPriceCount);
+    doc.autoTable(col1, rows1, { startY: 220 }); 
+     doc.text(250,270+30*rows1.length,'TOTAL ITEM: '+totalItemCount);
+    doc.text(250,290+30*(rows1.length),'TOTAL PRICE: '+totalPriceCount);
     doc.save('generated.pdf') 
   }
   const handleAddButtonClick = () => {
@@ -63,8 +80,17 @@ const App = () => {
       price: inputPrice,
       quantity: inputQuantity,
       // isSelected: false,
+     
     };
-
+     if(newItem.price<0||newItem.price==='')
+      {
+        alert('ENTER VALID PRICE');
+      }
+      else if(newItem.quantity<0||newItem.quantity==='')
+      alert('ENTER VALID QUANTITY')
+      else if(newItem.itemName==='')
+      alert('ENTER VALID ITEM')
+      else{
     const newItems = [...items, newItem];
 
     setItems(newItems);
@@ -74,10 +100,12 @@ const App = () => {
     setTotalItemCount('');
     
   };
+  }
   useEffect(() => {
     calculateTotal();
   },[items]);
 
+  
   const handleQuantityIncrease = (index) => {
     const newItems = [...items];
 
@@ -137,14 +165,19 @@ const App = () => {
           <div className='add-item-input-new' id='hide'>  Add your grocery items</div>
           <div className='add-item-box'>
             <span className='input-change'>
-            <input  id='hide' value={inputValue} onChange={(event) => {setInputValue(event.target.value)}} className='add-item-input' placeholder='Add an item' type='text' />
+            <input  id='hide' value={inputValue} onChange={(event) => {
+              
+              setInputValue(event.target.value)}} className='add-item-input' placeholder='Add an item' type='text' />
             </span>
             <span className='input-change'>
-            <input id='hide' value={inputPrice} onChange={(event) => setInputPrice(event.target.value)} className='add-item-input' placeholder='Add price' type='number' />
+            <input id='hide' value={inputPrice} onChange={(event) => {
+              
+              setInputPrice(event.target.value)}} className='add-item-input' placeholder='Add price' type='number' />
             </span>
             <span className='input-change'>
             <input id='hide' value={inputQuantity} onChange={(event) => setTotalQuantityCount(event.target.value)} className='add-item-input' placeholder='Quantity' type='number' />
             </span>
+           
             <FontAwesomeIcon id='hide' icon={faPlus} onClick={() => handleAddButtonClick()} />
           </div>
           
